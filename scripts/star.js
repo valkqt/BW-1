77d40a335b13b785ac2778665b2ca0a61440e85b
FullMetalMisso <sr1163374794@live.com>
2023-11-14 18:49:18 +0100

    aggiunte funzionalitá illuminazione stelle, incompleto il click

--- scripts/star.js
+++ scripts/star.js
@@ -1,14 +1,64 @@
+let isMouseOutEnabled = true;
 
 function mouseOver() {
-    let selezioneStella = document.querySelectorAll(".star")
+    let selezioneStella = document.querySelectorAll(".star");
+
     for (let i = 0; i < selezioneStella.length; i++) {
         selezioneStella[i].addEventListener("mouseover", () => {
+            selezioneStella[i].setAttribute("src", "assets/star.svg");
+
+            for (let j = 0; j < i; j++) {
+                selezioneStella[j].setAttribute("src", "assets/star.svg");
+            }
+        });
+    }
+
+    let rate = 0;
+
+    for (let i = 0; i < selezioneStella.length; i++) {
+        selezioneStella[i].addEventListener("click", () => {
+            rate = i;
             
-            selezioneStella[i].setAttribute("src", "assets/star.svg")
-            for(let j= 0; j < i; j++) {
-                selezioneStella[j].setAttribute("src", "assets/star.svg")
+        });
+    }
+
+    return rate; 
+}
+
+function mouseOut() {
+    if (!isMouseOutEnabled) return;
+
+    let selezioneStella = document.querySelectorAll(".star");
+
+    for (let i = 0; i < selezioneStella.length; i++) {
+        selezioneStella[i].addEventListener("mouseout", () => {
+            selezioneStella[i].setAttribute("src", "assets/darkStar.svg");
+
+            for (let j = 0; j < i; j++) {
+                selezioneStella[j].setAttribute("src", "assets/darkStar.svg");
             }
         });
     }
 }
-mouseOver()
+
+function clickStill() {
+    let selezioneStella = document.querySelectorAll(".star");
+
+    for (let i = 0; i < selezioneStella.length; i++) {
+        selezioneStella[i].addEventListener("click", () => {
+            selezioneStella[i].setAttribute("src", "assets/star.svg");
+
+            for (let j = 0; j < i; j++) {
+                selezioneStella[j].setAttribute("src", "assets/star.svg");
+            }
+        });
+    }
+}
+
+// Disable mouseOut functionality
+isMouseOutEnabled = false;
+
+// Call the functions
+mouseOver();
+mouseOut();
+clickStill();
