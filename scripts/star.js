0a23c77c650b7babfbb643ef40e0ed08903ed7c8
FullMetalMisso <sr1163374794@live.com>
2023-11-16 12:02:39 +0100

    aggiunta funzionalitá commento, perfezionato css per centrare tutto

--- scripts/star.js
+++ scripts/star.js
@@ -1,10 +1,12 @@
 let punteggioAttuale = 0;
+let valoreX 
 
 function highlightStar(starNumber) {
   for (let i = 1; i <= starNumber; i++) {
     const star = document.querySelector(`.star:nth-child(${i})`);
     star.src = "assets/star.svg";
   }
+  valoreX = starNumber
 }
 
 function resetStars() {
@@ -18,11 +20,31 @@ function resetStars() {
   }
 }
 
-  
-
 function rateStar(punteggio) {
   punteggioAttuale = punteggio;
- 
-
   resetStars();
 }
+
+function generaCommento() {
+ const prendiCommento = document.querySelector(".commentRate").value
+ const prendiRecensione = document.getElementById("recensioneUtente")
+ const spazioRecensione = document.createElement("p")
+ const spazioStelle = document.createElement("div")
+ spazioStelle.classList.add("divRecensione")
+ prendiRecensione.appendChild(spazioStelle)
+ for(let i = 0; i < valoreX; i++){
+  let stella = document.createElement("img")
+  stella.src = "assets/star.svg"
+  spazioStelle.appendChild(stella)
+  stella.style.transform = "scale(0.5)"
+}
+ prendiRecensione.appendChild(spazioRecensione)
+ spazioRecensione.innerText = prendiCommento
+}
+
+
+const richiamoCommento = document.querySelector(".bottoneIlluminato")
+richiamoCommento.addEventListener("click", generaCommento)
+
+
+
